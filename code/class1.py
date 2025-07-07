a09272a06bdbd187e2a0978ce2f4701d1ac13ef9
GavianoGabs <gabriella.gt@gmail.com>
2025-07-07 15:20:52 -0300

    Adding classes generated from StarUML

---
+++ code/class1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Class1:
+    def __init__(self):
+        self.window = None
+
+    def run(self, ):
+        pass
---
+++ code/entity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Class3 import Class3
+
+
+class Entity(Class3):
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
